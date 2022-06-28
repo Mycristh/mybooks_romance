621384cc3d503fe8e7ffe5683a1039be135ae4fa
Mylena Oliveira <101020021+Mycristh@users.noreply.github.com>
2022-06-28 14:10:49 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,21 @@
+var listaLivros = [
+  "https://images-na.ssl-images-amazon.com/images/I/A1nnf-d831L.jpg",
+  "https://images-na.ssl-images-amazon.com/images/I/81UQbG8iL4L.jpg",
+  "https://images-na.ssl-images-amazon.com/images/I/71leiloc95L.jpg",
+  "https://m.media-amazon.com/images/I/51w3pD7FCYL.jpg",
+  "https://images-na.ssl-images-amazon.com/images/I/51uY-e56LnL._SX321_BO1,204,203,200_.jpg",
+  "https://images-na.ssl-images-amazon.com/images/I/81JsfLmHPmL.jpg",
+  "https://images-na.ssl-images-amazon.com/images/I/61jp16oF+lL.jpg",
+  "https://m.media-amazon.com/images/I/41UVvYCfqmL.jpg",
+  "https://images-na.ssl-images-amazon.com/images/I/71jeQnaNsJL.jpg",
+  "https://images-na.ssl-images-amazon.com/images/I/91lxxIREpeL.jpg",
+  "https://img.travessa.com.br/livro/GR/3f/3f9c55ed-ad06-476d-88f4-a9f2452375bf.jpg",
+  "https://images-na.ssl-images-amazon.com/images/I/A1byrycCrzL.jpg",
+  "https://images-na.ssl-images-amazon.com/images/I/61Bqmx59inL.jpg",
+  "https://images-na.ssl-images-amazon.com/images/I/41kad3B6UHS._SX346_BO1,204,203,200_.jpg",
+  "https://images-na.ssl-images-amazon.com/images/I/91BLKruONSL.jpg"
+];
+
+for (var i = 0; i < listaLivros.length; i++) {
+  document.write("<img src=" + listaLivros[i] + ">");
+}
